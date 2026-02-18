1d9c2487051141fa48d3ba67471dbc7a8c555951
F-M-Mada-Dziban <63380357+F-M-Mada-Dziban@users.noreply.github.com>
2026-02-18 15:04:03 +0700

    Update bujurSangkar.java

--- Latihan_120226/bujurSangkar.java
+++ Latihan_120226/bujurSangkar.java
@@ -1,15 +1,14 @@
 class bujurSangkar extends bidangDuaDimensi {
     private double sisi;
 
-    // Constructor
     public bujurSangkar(String nama, double sisi) {
         super(nama);
         this.sisi = sisi;
     }
 
-    // Override method luas
     @Override
     public double luas() {
         return sisi * sisi;
     }
 }
+
