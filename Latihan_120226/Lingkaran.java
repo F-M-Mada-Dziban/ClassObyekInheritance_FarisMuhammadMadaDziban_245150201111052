3e105bc084e695b1c0305b4515e478dea1837144
F-M-Mada-Dziban <63380357+F-M-Mada-Dziban@users.noreply.github.com>
2026-02-18 15:03:41 +0700

    Update Lingkaran.java

--- Latihan_120226/Lingkaran.java
+++ Latihan_120226/Lingkaran.java
@@ -1,15 +1,14 @@
 class Lingkaran extends bidangDuaDimensi {
     private double jariJari;
 
-    // Constructor
     public Lingkaran(String nama, double jariJari) {
         super(nama);
         this.jariJari = jariJari;
     }
 
-    // Override method luas
     @Override
     public double luas() {
         return Math.PI * jariJari * jariJari;
     }
 }
+
